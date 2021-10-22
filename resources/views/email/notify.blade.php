fb6987c4671f5e1e679c9ad6d7ca76a450e91f7e
shenter <shenter@yandex.ru>
2021-10-22 14:46:12 +0300

    Add mail and queue table

---
+++ resources/views/email/notify.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New feedback from user</title>
+</head>
+<body>
+
+<h1>This is test mail from Tutsmake.com</h1>
+<p>Laravel 8 send email example</p>
+
+</body>
+</html>
